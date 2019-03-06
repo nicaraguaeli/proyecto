4fe45e31802c8a2f2007729aa17cfe7f43419bae
nicaraguaeli <moncadaeli567@gmail.com>
2019-03-06 10:59:59 -0600

    vinculos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,11 +85,11 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://laravel.com/docs">Eli</a>
+                    <a href="https://laracasts.com">Jose</a>
+                    <a href="https://laravel-news.com">Moncada</a>
+                    <a href="https://blog.laravel.com">Galeano</a>
+                    <a href="https://nova.laravel.com">Rama2</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
